0b5832c1e7e0755cccbb3b8770b770eef2eda50c
MedDhiia <Med.Dhiia@Gmail.com>
2020-11-24 09:35:16 +0100

    Day One Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from './models/person.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  selectedPerson ?: Person;
+  persons:Array<Person> = [
+    {
+      firstname: 'Ahmed',
+      lastname: 'Ahmed'
+    },
+    {
+      firstname: 'Dhiaa',
+      lastname: 'Dhiaa'
+    },
+    {
+      firstname: 'Alaa',
+      lastname: 'Namouchi'
+    },
+    {
+      firstname: 'Mahmoud',
+      lastname: 'Mahmoud'
+    }
+  ];
+
+  deletePerson(index: number) {
+    this.persons.splice(index, 1);
+  }
+
+  showPerson(index: number) {
+    this.selectedPerson = this.persons[index];
+  }
+
+  clickedPerson(person: Person){
+    console.log(person);
+  }
 }
